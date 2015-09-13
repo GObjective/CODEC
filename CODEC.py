d76700606c9ed7c32f44af26cc8b79f84cb49727
Ipek <harun.ipek@gmail.com>
2015-09-14 00:45:26 +0300

    Update CODEC.py
    Optimized

--- CODEC.py
+++ CODEC.py
@@ -1,23 +1,29 @@
 if True:
-  if True:
-    from os import system
-  if True:
-    from sys import argv
-    argc = len(argv)  
-  
+  from os import system
 if True:
-  if(argc != 1 and argc < 2):
-    if(argv[1] == "-C"):
-      if(argv[2] != None):
-        system("CDC -C " + argv[2])
-      else:
-        print("\033[91m" + "Error: [No input file]" + "\033[0m")
-    elif(argv[1] == "-D"):
-      if(argv[2] != None):
-        system("CDC -D " + argv[2])
-      else:
-        print("\033[91m" + "Error: [No output file]" + "\033[0m") 
-    else:
-      print("\033[91m" + "Error: [Unknown argument]" + "\033[0m")
+  from sys import exit as STOPS, argv
+
+if True:
+  if(len(argv) == 1):
+    print("Usage: CODEC <-C/D> <Input/Output file>")
+    STOPS(0)
   else:
-    print("\033[91m" + "Error: [No such argument or so much arguments]" + "\033[0m")
+    if(len(argv) > 3):
+      print("Error: So much arguments")
+      STOPS(0)
+    else:
+      if(argv[1] == "-C"):
+        if(argv[2] != None):
+          system("CDC -C" + str(argv[2]))
+        else:
+          print("Error: No output file")
+          STOPS(0)
+      elif(argv[1] == "-D"):
+        if(argv[2] != None):
+          system("CDC -D" + str(argv[2]))
+        else:
+          print("Error: No input file")
+          STOPS(0)
+      else: 
+        print("Error: Unknown argument")
+        STOPS(0)
